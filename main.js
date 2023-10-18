e3895dd6a5fdca80320bf06b0ae6eb0c6073f0bd
TAskMAster339 <dimagr3214@gmail.com>
2023-10-18 23:37:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+const list = document.getElementById('list');
+let isShown = false
+
+function toggleListVisibility(){
+    if (isShown){
+        list.style.display = 'none';
+        isShown = false
+    }else{
+        list.style.display = 'block';
+        isShown = true
+    }
+}
+
+btn.addEventListener('click', toggleListVisibility);
